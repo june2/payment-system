049d30028c549c52754cf1ecd7c48bba0cec7f56
june <kimjy4536@hotmail.com>
2021-01-02 03:14:35 +0900

    add redis-lock

--- src/main/java/com/kakao/pay/config/LockConfig.java
+++ src/main/java/com/kakao/pay/config/LockConfig.java
@@ -4,25 +4,9 @@ import com.kakao.pay.util.LockerUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.integration.jdbc.lock.DefaultLockRepository;
-import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
-import org.springframework.integration.jdbc.lock.LockRepository;
-import org.springframework.integration.support.locks.LockRegistry;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class LockConfig {
-    @Bean
-    public LockRepository lockRepository(DataSource dataSource) {
-        return new DefaultLockRepository(dataSource);
-    }
-
-    @Bean
-    public LockRegistry lockRegistry(LockRepository repository) {
-        return new JdbcLockRegistry(repository);
-    }
-
     @Bean
     public LockerUtil locker(RedisConnectionFactory connectionFactory ) {
         return new LockerUtil( connectionFactory, "cardinfo" );
